84498cf961b939f54b962dd730041b68199f8dd7
Andreas Dutzler <a.dutzler@gmail.com>
2023-06-28 23:40:57 +0200

    Update __init__.py

--- src/hyperelastic/__init__.py
+++ src/hyperelastic/__init__.py
@@ -1,4 +1,4 @@
-from . import frameworks, math, spaces, models
+from . import frameworks, math, models, spaces
 from .__about__ import __version__
 
 __all__ = ["spaces", "frameworks", "math", "models", "__version__"]

134e3d0039409485c1a1ac4d2aa1d91273637ab3
Andreas Dutzler <a.dutzler@gmail.com>
2023-07-04 00:49:26 +0200

    `Ogden`: Change to list of arguments

--- src/hyperelastic/models/stretches/_ogden.py
+++ src/hyperelastic/models/stretches/_ogden.py
@@ -1,14 +1,23 @@
+import numpy as np
+
+
 class Ogden:
     def __init__(self, mu, alpha):
         self.mu = mu
         self.alpha = alpha
 
     def gradient(self, stretches, statevars):
-        dWdλ = 2 * self.mu / self.alpha * stretches ** (self.alpha - 1)
-        return dWdλ, statevars
+        dWdλα = np.zeros_like(stretches)
+
+        for mu, alpha in zip(self.mu, self.alpha):
+            dWdλα += 2 * mu / alpha * stretches ** (alpha - 1)
+
+        return dWdλα, statevars
 
     def hessian(self, stretches, statevars):
-        d2Wdλdλ = (
-            2 * self.mu / self.alpha * (self.alpha - 1) * stretches ** (self.alpha - 2)
-        )
-        return [*d2Wdλdλ, None, None, None]
+        d2Wdλαdλα = np.zeros_like(stretches)
+
+        for mu, alpha in zip(self.mu, self.alpha):
+            d2Wdλαdλα += 2 * mu / alpha * (alpha - 1) * stretches ** (alpha - 2)
+
+        return [*d2Wdλαdλα, None, None, None]

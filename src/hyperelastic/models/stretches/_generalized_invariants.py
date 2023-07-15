1feb45e9326af6311ceb925ec01715371c6ac5b3
Andreas Dutzler <a.dutzler@gmail.com>
2023-07-16 01:01:21 +0200

    Update _generalized_invariants.py

--- src/hyperelastic/models/stretches/_generalized_invariants.py
+++ src/hyperelastic/models/stretches/_generalized_invariants.py
@@ -1,33 +1,6 @@
 import numpy as np
 
 
-def strain(stretch, exponent):
-    "Seth-Hill strain as a function of the stretch (and first and second derivatives)."
-
-    k = exponent
-    λ = stretch
-
-    E = (λ**k - 1) / k
-    dEdλ = λ ** (k - 1)
-    d2Edλdλ = (k - 1) * λ ** (k - 2)
-
-    return E, dEdλ, d2Edλdλ
-
-
-def deformation(stretch, exponent):
-    """Generalized deformation as a function of the stretch (and first and second
-    derivatives)."""
-
-    k = exponent
-    λ = stretch
-
-    Ck = 2 / k * λ**k
-    dCkdλ = 2 * λ ** (k - 1)
-    d2Ckdλdλ = 2 * (k - 1) * λ ** (k - 2)
-
-    return Ck, dCkdλ, d2Ckdλdλ
-
-
 class GeneralizedInvariantsModel:
     r"""The Model-Framework for a Total-Lagrangian generalized invariant-based
     isotropic hyperelastic material formulation provides the material behaviour-

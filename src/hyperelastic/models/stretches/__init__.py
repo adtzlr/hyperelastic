307fffb90b036dd8374e12b247d9876aa8b3fe39
Andreas Dutzler <a.dutzler@gmail.com>
2023-07-16 00:55:09 +0200

    Update __init__.py

--- src/hyperelastic/models/stretches/__init__.py
+++ src/hyperelastic/models/stretches/__init__.py
@@ -1,4 +1,4 @@
 from ._ogden import Ogden
-from ._strain import GeneralizedInvariantsModel, deformation, strain
+from ._generalized_invariants import GeneralizedInvariantsModel, deformation, strain
 
 __all__ = ["Ogden", "GeneralizedInvariantsModel", "strain", "deformation"]
